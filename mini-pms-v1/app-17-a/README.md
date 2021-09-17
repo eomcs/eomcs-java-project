477cc10face225a0cb07ed957f82a5d1bb394d62
eomjinyoung <jinyoung.eom@gmail.com>
2021-09-17 10:58:28 +0900

    17-a

---
+++ mini-pms-v1/app-17-a/README.md
@@ -0,0 +1,32 @@
+# 17-a. 메뉴 리팩토링: 상세보기에서 변경, 삭제 기능 수행하기
+
+이번 훈련에서는 객체 간의 종속성을 줄이는 방법을 알아본다.
+
+
+## 훈련 목표
+
+-
+
+## 훈련 내용
+
+- 
+
+
+## 실습
+
+
+### 1단계 - 객체 간의 종속성 이해하기
+
+- 게시글 상세 보기에서 게시글 변경, 삭제를 수행하려면,
+  - BoardDetailHandler 는 BoardUpdateHandler와 BoardDeleteHandler를 알아야 한다.
+  - 즉 인스턴스 필드에 사용할 객체를 담고 있어야 한다.
+  - BoardDetailHandler는 BoardUpdateHandler와 BoardDeleteHandler 없이 사용할 수 없다.
+  - 이것이 '종속된다'는 의미다.
+
+- com.eomcs.pms.handler.BoardDetailHandler 변경
+  - 생성자에서 BoardUpdateHandler와 BoardDeleteHandler 객체를 파라미터로 받아 보관 한다.
+  - 상세보기에서 게시글 변경, 삭제를 수행할 수 있도록 변경한다.
+- com.eomcs.pms.App 클래스 변경
+  - BoardDetailHandler의 변경에 맞춰 의존 객체를 파라미터로 넘긴다.
+  - 게시글 변경, 삭제 메뉴를 제거한다.
+
---
+++ mini-pms-v1/app-17-a/src/main/java/com/eomcs/pms/handler/BoardDetailHandler.java
@@ -0,0 +1,73 @@
+package com.eomcs.pms.handler;
+
+import java.util.List;
+import com.eomcs.pms.domain.Board;
+import com.eomcs.pms.domain.Member;
+import com.eomcs.util.Prompt;
+
+public class BoardDetailHandler extends AbstractBoardHandler {
+
+  BoardUpdateHandler boardUpdateHandler;
+  BoardDeleteHandler boardDeleteHandler;
+
+  public BoardDetailHandler(
+      List<Board> boardList, 
+      BoardUpdateHandler boardUpdateHandler,
+      BoardDeleteHandler boardDeleteHandler) {
+    super(boardList);
+    this.boardUpdateHandler = boardUpdateHandler;
+    this.boardDeleteHandler = boardDeleteHandler;
+  }
+
+  @Override
+  public void execute() {
+    System.out.println("[게시글 상세보기]");
+    int no = Prompt.inputInt("번호? ");
+
+    Board board = findByNo(no);
+
+    if (board == null) {
+      System.out.println("해당 번호의 게시글이 없습니다.");
+      return;
+    }
+
+    System.out.printf("제목: %s\n", board.getTitle());
+    System.out.printf("내용: %s\n", board.getContent());
+    System.out.printf("작성자: %s\n", board.getWriter().getName());
+    System.out.printf("등록일: %s\n", board.getRegisteredDate());
+
+    board.setViewCount(board.getViewCount() + 1);
+    System.out.printf("조회수: %d\n", board.getViewCount());
+    System.out.println();
+
+    Member loginUser = AuthLoginHandler.getLoginUser(); 
+    if (loginUser == null || board.getWriter().getNo() != loginUser.getNo()) {
+      return;
+    }
+
+    while (true) {
+      String input = Prompt.inputString("변경(U), 삭제(D), 이전(0)>");
+      switch (input) {
+        case "U":
+        case "u":
+          boardUpdateHandler.execute();
+          return;
+        case "D":
+        case "d":
+          boardDeleteHandler.execute();
+          return;
+        case "0":
+          return;
+        default:
+          System.out.println("명령어가 올바르지 않습니다!");
+      }
+    }
+  }
+}
+
+
+
+
+
+
+
